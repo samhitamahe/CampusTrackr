614d7426ee926aa0dc7ca25890a565d3dbab2a65
Samhita M <samhita162004@gmail.com>
2025-08-04 20:03:32 +0530

    Initial commit - full stack project

---
+++ api-main/config/corsOptions.js
@@ -0,0 +1,42 @@
+// const allowedOrigins = [
+//   "https://kollege.onrender.com",
+//   "http://localhost:3000",
+// ];
+
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (
+//       allowedOrigins.indexOf(origin) !== -1
+//       //! remove in production
+//       // || !origin
+//     ) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not Allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+//   optionsSuccessStatus: 200,
+// };
+
+// module.exports = corsOptions;
+
+// config/corsOptions.js
+const allowedOrigins = [
+  "http://localhost:3000", // Your frontend
+  // "http://127.0.0.1:3000",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not Allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
